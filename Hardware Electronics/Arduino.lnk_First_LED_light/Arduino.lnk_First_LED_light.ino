39a61fec3a2b140b50a3f3af224689ec4b273754
kanini-esther <61290714+kanini-esther@users.noreply.github.com>
2020-06-05 21:08:03 +0300

    Add files via upload

---
+++ Hardware Electronics/Arduino.lnk_First_LED_light/Arduino.lnk_First_LED_light.ino
@@ -0,0 +1,46 @@
+int Red=3;
+int Blue=3; 
+int Green=3;
+int Yellow=3;
+int Amber=3;
+int Purple=3;
+int Violet=3;
+void setup(){
+  pinMode(Red.OUTPUT);
+  pinMode(Blue.OUTPUT);
+  pinMode(Green,OUTPUT);
+  pinMode(Yellow,OUTPUT);
+  pinMode(Amber,OUTPUT);
+  pinMode(Purple,OUTPUT);
+  pinMode(Violet,OUTPUT);
+  
+}
+void loop(){
+  digitalWrite(Red,HIGH);
+  delay(500);
+  digitalWrite(Red,LOW);
+  delay(500);
+  digitalWrite(Blue,HIGH);
+  delay(500);
+  digitalWrite(Blue,LOW);
+  delay(500);
+  digitalWrite(Green,HIGH);
+  delay(500);
+  digitalWrite(Green,LOW);
+  delay(500);
+  digitalWrite(Amber,HIGH);
+  delay(500);
+  digitalWrite(Amber,LOW);
+  delay(500);
+  digitalWrite(Purple,HIGH);
+  delay(500);
+  digitalWrite(Purple,LOW);
+  delay(500);
+  digitalWrite(Violet,HIGH);
+  delay(500);
+  digitalWrite(Violet,LOW);
+  delay(500);
+}
+  
+
+ 
